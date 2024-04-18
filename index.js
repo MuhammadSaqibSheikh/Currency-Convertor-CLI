627608608fcbf8a9650c97199084c64a336e354c
[Saqib-Sheikh] <[saqisheikh86@gmail.com]>
2024-04-18 15:03:11 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import inquirer from 'inquirer';
+const currancy = {
+    USD: 1,
+    EUR: 0.91,
+    GBR: 0.76,
+    INR: 78.50,
+    PKR: 288,
+};
+let user_answer = await inquirer.prompt([{
+        name: "from",
+        message: " Enter from currancy",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "To",
+        message: " Enter To currancy",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: "Enter your amount"
+    }
+]);
+let fromAmount = currancy[user_answer.from];
+let toAmount = currancy[user_answer.To];
+let amount = user_answer.amount;
+let Basedcurrancy = amount / fromAmount;
+let convertedAmount = Basedcurrancy * toAmount;
+console.log(convertedAmount);
